acc5dbedcca2a3c2aa629a0152d9e818bdd34827
Mones26 <jageitos@andrew.cmu.edu>
2025-10-24 23:53:35 -0400

    Not working, connecting to the vercel link is not working

--- app/main.py
+++ app/main.py
@@ -12,11 +12,8 @@ def read_root():
 RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
                     "c": "11:00~11:50", "d": "1:00~1:50",
                     "e": "2:00~2:50", "f": "3:00~3:50"}
-RECITATION_TAs = {"a": ["Juan", "---"], "b": ["Kareem", "Jonathan"],
-                    "c": ["Juan", "Rathi"], "d": ["Sarah", "Savannah"],
-                    "e": ["Sarah", "Rathi"], "f": ["Bing", "Jonathan"]}
 
-MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
+MICROSERVICE_LINK = "https://f25-docker-recitation-8jvfc6jzu-juans-projects-a6ad89e9.vercel.app/section_info/"
 
 
 @app.get("/section_info/{section_id}")
@@ -27,25 +24,31 @@ def get_section_info(section_id: str):
 
     section_id = section_id.lower()
 
-    # response = requests.get(MICROSERVICE_LINK + section_id)
+    response = requests.get(MICROSERVICE_LINK + section_id)
 
-    # # You can check out what the response body looks like in terminal using the print statement
-    # data = response.json()
-    # print(data)
-    # ta_name_list = data["ta_names"]
-    # ta1_name = ta_name_list[0]["fname"] + " " + ta_name_list[0]["lname"]
-    # ta2_name = ta_name_list[1]["fname"] + " " + ta_name_list[1]["lname"]
+    # You can check out what the response body looks like in terminal using the print statement
+    # print(response)
+    data = response.json()
+    print(data)
+    ta_name_list = data["ta"]
+    ta1_name = ta_name_list[0]
+    ta2_name = ta_name_list[1]
+
+    start_time = data["start_time"]
+    end_time = data["end_time"]
+
+    # OR 
+    # start_time, end_time = RECITATION_HOURS[section_id].split("~")
 
     # print(ta1_name)
-
     # TODO
     
     if section_id in RECITATION_HOURS:
         return {
             "section": section_id,
-            "start_time": RECITATION_HOURS[section_id].split("~")[0],
-            "end_time": RECITATION_HOURS[section_id].split("~")[1],
-            "ta": RECITATION_TAs[section_id]
+            "start_time": start_time,
+            "end_time": end_time,
+            "ta": [ta1_name, ta2_name]
         }
     else:
         raise HTTPException(status_code=404, detail="Invalid section id")
